713454610169157eb2995253caa8c98687d13f62
giorgi mikeladze <mikeladzegiorgi538@outlook.com>
2023-11-18 12:04:47 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+int numb;
+Console.Write("Enter integer value: ");
+bool is_parsed = Int32.TryParse(Console.ReadLine(), out numb);
+
+if (is_parsed && numb >= 1)
+{
+    Console.Write($"Divisors of {numb} are: ");
+    for (int i = 1; i <= numb; i++)
+	{
+       
+        if (numb%i==0) {
+            Console.Write($" {i} ");
+        }
+	}
+}
+else {
+    Console.WriteLine("invlid value!!!");
+}
